12b962009ae9973126c2f5329a2599a0a4fe3dc5
Harshita <harshita@mantralabsglobal.com>
2017-07-18 14:46:10 +0530

    DCV | Changes & Addition 1. Changes in index.php 2. Addititon of jQuery and Bootstrap files

--- index.php
+++ index.php
@@ -4,7 +4,38 @@
  * Author:      Harshita
  * Place:       Gurgaon
  */
-
-
-echo 'Hi, this is test commit for the project. I hope it works.'
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Introduction</title>
+        <style type="text/css">
+            .cv-body{
+                background-color: darkcyan;
+            }
+            
+            .cv-heading1{
+                font: 200px fantasy;
+                color: white;
+                text-align: center;
+                
+            }
+        </style>
+        <script src="<?php echo __DIR__; ?>/external/jQuery/jquery-3.2.1.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+    </head>
+    <body class="cv-body" onload="cv_animation_underline()">
+        <div id="introduction" ng-app="introduction">
+            <div class="cv-heading1"> ILLUSTRATED <span id="heading_animation"> STORY </span> </div>
+        </div>
+        <div class="glyphicon glyphicon-circle-arrow-down"></div>
+    </body>
+    
+    <script text="text/javascript">
+        function cv_animation_underline(){
+            document.getElementById("heading_animation").style.color = "red";
+            document.getElementById("heading_animation").style.transition = "all 5s";
+        }
+    </script>
+</html>
