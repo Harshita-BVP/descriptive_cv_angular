e1528e7721a7672b242ee452532ad1e9af2e4eae
Harshita <harshita@mantralabsglobal.com>
2017-07-26 18:57:51 +0530

    DCV Changes in landing & introduction section

--- index.php
+++ index.php
@@ -9,33 +9,43 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>Introduction</title>
-        <style type="text/css">
-            .cv-body{
-                background-color: darkcyan;
-            }
-            
-            .cv-heading1{
-                font: 200px fantasy;
-                color: white;
-                text-align: center;
-                
-            }
-        </style>
-        <script src="<?php echo __DIR__; ?>/external/jQuery/jquery-3.2.1.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+        <link href="external/bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/default.css" rel="stylesheet">
     </head>
     <body class="cv-body" onload="cv_animation_underline()">
-        <div id="introduction" ng-app="introduction">
-            <div class="cv-heading1"> ILLUSTRATED <span id="heading_animation"> STORY </span> </div>
+        <div id="landing">
+            <div class="cv-heading1"> ILLUSTRATIVE <div id="heading_animation"> CV </div> </div>
+            <a class="glyphicon glyphicon-circle-arrow-down gi-3x" id="down_arrow" href="#introduction"></a>
+            
+            <div id="introduction" class="introduction_content">
+                <img src="images/harshita.png" width="250px" height="250px" style="float: right; border: 5px solid #eee; border-radius: 5px;"/>
+                <h1 style="float: left;"> Ahoy<span class="glyphicon glyphicon-exclamation-sign"></span> &nbsp;&nbsp; I am Harshita. </h1> 
+                <div style="clear: both;"></div>
+                <br><br>
+                I am a Full-Stack Developer. This website elaborates about my career, education and hobbies. You can navigate through all sections provided below.
+                <br>
+                Hope you like it.
+            </div>
         </div>
-        <div class="glyphicon glyphicon-circle-arrow-down"></div>
+        <nav>
+            <ul class="nav nav-justified nav-link">
+                <li><a>Hi, This is me. <span class="glyphicon glyphicon-queen"></span></a></li>
+                <li><a>Work Road <span class="glyphicon glyphicon-road"></span></a></li>
+                <li><a>Educational Qualifications <span class="glyphicon glyphicon-education"></span></a></li>
+            </ul>
+        </nav>
     </body>
+    
+    
+    <script src="external/jQuery/jquery-3.2.1.js"></script>  
+    <script src="external/bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
     
     <script text="text/javascript">
         function cv_animation_underline(){
             document.getElementById("heading_animation").style.color = "red";
-            document.getElementById("heading_animation").style.transition = "all 5s";
+            document.getElementById("heading_animation").style.transition = "color 5s";
         }
     </script>
 </html>
